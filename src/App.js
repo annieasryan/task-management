8e635066c1e046a0200dd25015a664cee4634a42
annieasryan <msasryan88@gmail.com>
2022-08-07 14:36:51 +0400

    redux-setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from "react";
+import './app.css'
+
 function App() {
-  return <div className="App">My App</div>;
+  return (
+    <div className="container-fluid">
+      <h4 className="text-light text-center text-uppercase">Task management platform</h4>
+      <div className="container">
+
+      </div>
+    </div>
+  );
 }
 
 export default App;
